69807d8939c83dc736298624a3340e8ff2702797
Jakub Šimek <simekjakub03@gmail.com>
2023-07-04 12:02:08 +0200

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -15,7 +15,7 @@ ax.axis([0, 10, 0, 10])
 
 
 # Initial position and velocities
-p1 = Particle(1,2,3,7,30)
+p1 = Particle(1,2,30,80,30)
 point, = ax.plot(int(p1.pos_x), int(p1.pos_y), "o", mfc = "none", markersize = p1.radius)
 
 prev_frame_time = 0
@@ -27,15 +27,16 @@ def update(frame):
     prev_frame_time = current_time
 
     p1.pos_x += dt * p1.vel_x
-    if 0 > p1.pos_x or 10 < p1.pos_x:
-            p1.vel_x = -p1.vel_x
     p1.pos_y += dt * p1.vel_y
-    if 0 > p1.pos_y or 10 < p1.pos_y:
+    
+    if (0 + p1.radius//35) > p1.pos_x or (10 - p1.radius//35) < p1.pos_x:
+            p1.vel_x = -p1.vel_x
+    if (0 + p1.radius//35) > p1.pos_y or (10 - p1.radius//35) < p1.pos_y:
             p1.vel_y = -p1.vel_y
     x = p1.pos_x
     y = p1.pos_y
     point.set_data(x, y)
     return point,
 
-ani = FuncAnimation(fig, update, interval=100, frames=np.arange(0, 1000))
+ani = FuncAnimation(fig, update, interval=1, frames=np.arange(0, 1000))
 plt.show()
